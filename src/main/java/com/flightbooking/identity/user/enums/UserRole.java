dc68618927ed991eab67d199e6044f4c59f0dfc4
Kholoud Shaarawii <kholoudshaarawy.sb@gmail.com>
2026-07-21 18:35:41 +0300

    implement F1.1 user model

---
+++ src/main/java/com/flightbooking/identity/user/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.flightbooking.identity.user.enums;
+
+public enum UserRole {
+    CUSTOMER,
+    AIRLINE_ADMIN,
+    SUPER_ADMIN
+}
---
+++ src/main/java/com/flightbooking/identity/user/enums/UserStatus.java
@@ -0,0 +1,7 @@
+package com.flightbooking.identity.user.enums;
+
+public enum UserStatus {
+    ACTIVE,
+    BLOCKED,
+    DEACTIVATED
+}

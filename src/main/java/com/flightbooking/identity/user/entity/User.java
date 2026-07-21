6045abcd93e80d3e524b69cf76168022616558bf
Kholoud Shaarawii <kholoudshaarawy.sb@gmail.com>
2026-07-21 18:46:10 +0300

    user role field mapping

--- src/main/java/com/flightbooking/identity/user/entity/User.java
+++ src/main/java/com/flightbooking/identity/user/entity/User.java
@@ -39,7 +39,7 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private UserRole userRole;
+    private UserRole role;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
